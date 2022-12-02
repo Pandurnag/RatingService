d860c7a6471661232a5f6b1667e8087b44a5b045
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-12-02 15:30:38 +0530

    RatingService

---
+++ src/main/java/com/lcwd/rating/controllers/RatingController.java
@@ -0,0 +1,45 @@
+package com.lcwd.rating.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lcwd.rating.entities.Rating;
+import com.lcwd.rating.services.RatingService;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+	
+	private RatingService ratingService;
+	
+	//create rating
+	@PostMapping
+	public ResponseEntity<Rating> create(@RequestBody Rating rating){
+		return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
+	}
+	//get all
+	@GetMapping
+	public ResponseEntity<List<Rating>> getRatings(){
+		return ResponseEntity.ok(ratingService.getRatings());
+     }
+	
+	//get all
+	@PostMapping
+	public ResponseEntity<List<Rating>> getRatingsByUserId(String userId){
+		return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
+	}
+	//get all
+	@GetMapping("/hotels/{hotelId}")
+	public ResponseEntity<Object> getRatingsByHotelId(@PathVariable String hotelId){
+		return ResponseEntity.ok(ratingService.getRatingsByHotelId(hotelId));
+
+}
+}
